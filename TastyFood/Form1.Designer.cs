29c2b718e12cce333cfd035e873eeb0d8d3fef18
yoseph flores romero <yoseph554@gmail.com>
2025-07-04 15:57:56 -0600

    agregando iconos

--- TastyFood/Form1.Designer.cs
+++ TastyFood/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
@@ -44,33 +45,67 @@
             button14 = new Button();
             textBox1 = new TextBox();
             button15 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            pictureBox11 = new PictureBox();
+            label1 = new Label();
+            pictureBox12 = new PictureBox();
+            pictureBox13 = new PictureBox();
+            pictureBox14 = new PictureBox();
+            pictureBox15 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(85, 78);
+            button1.Font = new Font("Segoe UI", 16F);
+            button1.Location = new Point(84, 219);
             button1.Name = "button1";
-            button1.Size = new Size(150, 46);
+            button1.Size = new Size(247, 61);
             button1.TabIndex = 0;
-            button1.Text = "Combo 1";
+            button1.Text = "Combo #1";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(85, 150);
+            button2.Font = new Font("Segoe UI", 16F);
+            button2.Location = new Point(411, 219);
             button2.Name = "button2";
-            button2.Size = new Size(150, 46);
+            button2.Size = new Size(296, 61);
             button2.TabIndex = 1;
-            button2.Text = "Combo 2";
+            button2.Text = "Combo #2";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(333, 52);
+            button3.Font = new Font("Segoe UI", 16F);
+            button3.Location = new Point(785, 208);
             button3.Name = "button3";
-            button3.Size = new Size(150, 46);
+            button3.Size = new Size(214, 68);
             button3.TabIndex = 2;
             button3.Text = "Pechuga";
             button3.UseVisualStyleBackColor = true;
@@ -78,9 +113,10 @@
             // 
             // button4
             // 
-            button4.Location = new Point(333, 126);
+            button4.Font = new Font("Segoe UI", 16F);
+            button4.Location = new Point(802, 461);
             button4.Name = "button4";
-            button4.Size = new Size(150, 46);
+            button4.Size = new Size(197, 71);
             button4.TabIndex = 3;
             button4.Text = "Cadera";
             button4.UseVisualStyleBackColor = true;
@@ -88,9 +124,10 @@
             // 
             // button5
             // 
-            button5.Location = new Point(56, 365);
+            button5.Font = new Font("Segoe UI", 16F);
+            button5.Location = new Point(200, 932);
             button5.Name = "button5";
-            button5.Size = new Size(232, 46);
+            button5.Size = new Size(363, 71);
             button5.TabIndex = 7;
             button5.Text = "Paquete Tortilla";
             button5.UseVisualStyleBackColor = true;
@@ -98,9 +135,10 @@
             // 
             // button6
             // 
-            button6.Location = new Point(56, 293);
+            button6.Font = new Font("Segoe UI", 16F);
+            button6.Location = new Point(173, 595);
             button6.Name = "button6";
-            button6.Size = new Size(228, 46);
+            button6.Size = new Size(370, 70);
             button6.TabIndex = 6;
             button6.Text = "Tajada con pollo";
             button6.UseVisualStyleBackColor = true;
@@ -108,9 +146,10 @@
             // 
             // button7
             // 
-            button7.Location = new Point(333, 265);
+            button7.Font = new Font("Segoe UI", 16F);
+            button7.Location = new Point(802, 932);
             button7.Name = "button7";
-            button7.Size = new Size(150, 46);
+            button7.Size = new Size(197, 66);
             button7.TabIndex = 5;
             button7.Text = "Pierna";
             button7.UseVisualStyleBackColor = true;
@@ -118,9 +157,10 @@
             // 
             // button8
             // 
-            button8.Location = new Point(333, 193);
+            button8.Font = new Font("Segoe UI", 16F);
+            button8.Location = new Point(802, 695);
             button8.Name = "button8";
-            button8.Size = new Size(150, 46);
+            button8.Size = new Size(197, 75);
             button8.TabIndex = 4;
             button8.Text = "Ala";
             button8.UseVisualStyleBackColor = true;
@@ -128,9 +168,10 @@
             // 
             // button9
             // 
-            button9.Location = new Point(517, 365);
+            button9.Font = new Font("Segoe UI", 16F);
+            button9.Location = new Point(1436, 470);
             button9.Name = "button9";
-            button9.Size = new Size(150, 46);
+            button9.Size = new Size(166, 62);
             button9.TabIndex = 11;
             button9.Text = "Salsa";
             button9.UseVisualStyleBackColor = true;
@@ -138,9 +179,10 @@
             // 
             // button10
             // 
-            button10.Location = new Point(577, 230);
+            button10.Font = new Font("Segoe UI", 16F);
+            button10.Location = new Point(1161, 208);
             button10.Name = "button10";
-            button10.Size = new Size(150, 46);
+            button10.Size = new Size(176, 65);
             button10.TabIndex = 10;
             button10.Text = "Granita";
             button10.UseVisualStyleBackColor = true;
@@ -148,9 +190,10 @@
             // 
             // button11
             // 
-            button11.Location = new Point(577, 150);
+            button11.Font = new Font("Segoe UI", 16F);
+            button11.Location = new Point(1436, 202);
             button11.Name = "button11";
-            button11.Size = new Size(150, 46);
+            button11.Size = new Size(166, 71);
             button11.TabIndex = 9;
             button11.Text = "Jugo N";
             button11.UseVisualStyleBackColor = true;
@@ -158,9 +201,10 @@
             // 
             // button12
             // 
-            button12.Location = new Point(577, 52);
+            button12.Font = new Font("Segoe UI", 16F);
+            button12.Location = new Point(1161, 470);
             button12.Name = "button12";
-            button12.Size = new Size(150, 46);
+            button12.Size = new Size(176, 62);
             button12.TabIndex = 8;
             button12.Text = "Fresco";
             button12.UseVisualStyleBackColor = true;
@@ -168,9 +212,10 @@
             // 
             // button13
             // 
-            button13.Location = new Point(828, 78);
+            button13.Font = new Font("Segoe UI", 16F);
+            button13.Location = new Point(1767, 219);
             button13.Name = "button13";
-            button13.Size = new Size(150, 46);
+            button13.Size = new Size(175, 79);
             button13.TabIndex = 12;
             button13.Text = "Factura";
             button13.UseVisualStyleBackColor = true;
@@ -178,9 +223,10 @@
             // 
             // button14
             // 
-            button14.Location = new Point(1062, 78);
+            button14.Font = new Font("Segoe UI", 16F);
+            button14.Location = new Point(1980, 219);
             button14.Name = "button14";
-            button14.Size = new Size(150, 46);
+            button14.Size = new Size(175, 79);
             button14.TabIndex = 13;
             button14.Text = "Pagar";
             button14.UseVisualStyleBackColor = true;
@@ -188,27 +234,189 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(787, 150);
+            textBox1.Location = new Point(1767, 395);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(526, 39);
+            textBox1.Size = new Size(388, 39);
             textBox1.TabIndex = 14;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // button15
             // 
-            button15.Location = new Point(333, 365);
+            button15.Font = new Font("Segoe UI", 16F);
+            button15.Location = new Point(1161, 705);
             button15.Name = "button15";
-            button15.Size = new Size(150, 46);
+            button15.Size = new Size(176, 65);
             button15.TabIndex = 15;
             button15.Text = "Papas";
             button15.UseVisualStyleBackColor = true;
             button15.Click += button15_Click;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(411, 18);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(298, 180);
+            pictureBox1.TabIndex = 16;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(56, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(275, 186);
+            pictureBox2.TabIndex = 17;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(216, 391);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(298, 180);
+            pictureBox3.TabIndex = 18;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(134, 690);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(313, 236);
+            pictureBox4.TabIndex = 19;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(867, 828);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(75, 82);
+            pictureBox5.TabIndex = 20;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(867, 367);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(53, 67);
+            pictureBox6.TabIndex = 21;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(867, 119);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(70, 52);
+            pictureBox7.TabIndex = 22;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(862, 586);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(80, 66);
+            pictureBox8.TabIndex = 23;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Image = (Image)resources.GetObject("pictureBox9.Image");
+            pictureBox9.Location = new Point(1205, 573);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(89, 107);
+            pictureBox9.TabIndex = 24;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.Image = (Image)resources.GetObject("pictureBox10.Image");
+            pictureBox10.Location = new Point(1145, 70);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(203, 128);
+            pictureBox10.TabIndex = 25;
+            pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            pictureBox11.Image = (Image)resources.GetObject("pictureBox11.Image");
+            pictureBox11.Location = new Point(1145, 306);
+            pictureBox11.Name = "pictureBox11";
+            pictureBox11.Size = new Size(193, 128);
+            pictureBox11.TabIndex = 26;
+            pictureBox11.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F);
+            label1.Location = new Point(1784, 333);
+            label1.Name = "label1";
+            label1.Size = new Size(116, 59);
+            label1.TabIndex = 27;
+            label1.Text = "Total";
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.Image = (Image)resources.GetObject("pictureBox12.Image");
+            pictureBox12.Location = new Point(1425, 58);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(193, 128);
+            pictureBox12.TabIndex = 28;
+            pictureBox12.TabStop = false;
+            // 
+            // pictureBox13
+            // 
+            pictureBox13.Image = (Image)resources.GetObject("pictureBox13.Image");
+            pictureBox13.Location = new Point(1425, 319);
+            pictureBox13.Name = "pictureBox13";
+            pictureBox13.Size = new Size(193, 128);
+            pictureBox13.TabIndex = 29;
+            pictureBox13.TabStop = false;
+            // 
+            // pictureBox14
+            // 
+            pictureBox14.Image = (Image)resources.GetObject("pictureBox14.Image");
+            pictureBox14.Location = new Point(1758, 58);
+            pictureBox14.Name = "pictureBox14";
+            pictureBox14.Size = new Size(193, 128);
+            pictureBox14.TabIndex = 30;
+            pictureBox14.TabStop = false;
+            // 
+            // pictureBox15
+            // 
+            pictureBox15.Image = (Image)resources.GetObject("pictureBox15.Image");
+            pictureBox15.Location = new Point(1980, 58);
+            pictureBox15.Name = "pictureBox15";
+            pictureBox15.Size = new Size(193, 128);
+            pictureBox15.TabIndex = 31;
+            pictureBox15.TabStop = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1345, 598);
+            ClientSize = new Size(2343, 1399);
+            Controls.Add(pictureBox15);
+            Controls.Add(pictureBox14);
+            Controls.Add(pictureBox13);
+            Controls.Add(pictureBox12);
+            Controls.Add(label1);
+            Controls.Add(pictureBox11);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
             Controls.Add(button15);
             Controls.Add(textBox1);
             Controls.Add(button14);
@@ -227,6 +435,21 @@
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -249,5 +472,21 @@
         private Button button14;
         private TextBox textBox1;
         private Button button15;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox11;
+        private Label label1;
+        private PictureBox pictureBox12;
+        private PictureBox pictureBox13;
+        private PictureBox pictureBox14;
+        private PictureBox pictureBox15;
     }
 }
